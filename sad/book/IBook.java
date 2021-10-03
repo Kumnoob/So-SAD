7da9be15dd0593fda269a6b149e9e573a68dc0d4
LIKHITBHUM LIKHITNGAM <71270604+Kumnoob@users.noreply.github.com>
2021-10-03 21:56:52 +0700

    Add files via upload

---
+++ sad/book/IBook.java
@@ -0,0 +1,7 @@
+package com.solid.book;
+
+public interface IBook {
+    String getTitle();
+    String getCurrentPage();
+    boolean turnToNextPage();
+}
---
+++ sad/book/IPubPrinter.java
@@ -0,0 +1,5 @@
+package com.solid.book;
+
+public interface IPubPrinter {
+    void printToFile();
+}
