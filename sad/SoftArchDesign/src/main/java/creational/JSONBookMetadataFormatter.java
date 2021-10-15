d99d86f2aff57d38a2f3fb4f14ae8d2e323da6d6
LIKHITBHUM LIKHITNGAM <71270604+Kumnoob@users.noreply.github.com>
2021-10-15 22:41:28 +0700

    Add files via upload

--- sad/SoftArchDesign/src/main/java/creational/JSONBookMetadataFormatter.java
+++ sad/SoftArchDesign/src/main/java/creational/JSONBookMetadataFormatter.java
@@ -10,39 +10,37 @@ import java.util.List;
 public class JSONBookMetadataFormatter implements BookMetadataFormatter {
 
     private JSONObject bookObject;
-    private JSONObject book;
+    private JSONObject bookInfo;
     private JSONArray bookArray;
 
-    public JSONBookMetadataFormatter() {
+    public JSONBookMetadataFormatter(){
         reset();
     }
 
     @Override
     public BookMetadataFormatter reset() {
-        // Please implement this method. You may create additional methods as you see fit.
         bookObject = new JSONObject();
         bookArray = new JSONArray();
         bookObject.put("Books",bookArray);
+
         return this;
     }
 
     @Override
     public BookMetadataFormatter append(Book b) {
-        // Please implement this method. You may create additional methods as you see fit.
         List<String> authors = new ArrayList<String>(Arrays.asList(b.getAuthors()));
-        book = new JSONObject();
-        book.put("ISBN", b.getISBN());
-        book.put("Authors",authors );
-        book.put("Title", b.getTitle());
-        book.put("Publisher", b.getPublisher());
-        bookArray.add(book);
-        
+        bookInfo = new JSONObject();
+        bookInfo.put("ISBN", b.getISBN());
+        bookInfo.put("Authors",authors );
+        bookInfo.put("Title", b.getTitle());
+        bookInfo.put("Publisher", b.getPublisher());
+        bookArray.add(bookInfo);
+
         return this;
     }
 
     @Override
     public String getMetadataString() {
-        // Please implement this method. You may create additional methods as you see fit.
         return bookObject.toString();
     }
 }
